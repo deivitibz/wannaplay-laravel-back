17543f62faf5d19d04b6a483692c31c33a9beecd
Deivit <rppre@gmail.com>
2021-10-26 10:21:27 +0200

    fixed migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $jugadores = Jugador::All();
+        // $jugadores = Jugador::All();
 
     }
 }
--- resources/views/jugadores.blade.php
+++ resources/views/jugadores.blade.php
@@ -12,7 +12,7 @@
         <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Eliminar</span></a>
     </div>
 </div>
-{{-- <div class="row">
+<div class="row">
     <div class="col">
         <table class="table table-dark table-striped table-hover d-inline-table">
             <tbody>
@@ -53,7 +53,7 @@
             </tbody>
         </table>
     </div>
-</div> --}}
+</div>
 
 
 
